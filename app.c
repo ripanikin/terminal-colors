e752162354f06c01eeda96ad03f98f1477a0f698
ripanikin <reportings101@gmail.com>
2025-11-18 06:47:35 +0000

    Completed main function implementation

--- app.c
+++ app.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-// TODO: Add the include directive for the custom library header file here
+#include "term_utils.h"
 
 int main(void) {
-    
-    // TODO: Call the function from the library to clear the screen
-    
-    // TODO: Call the function to print a divider line
+    clear_screen();
+    print_divider();
 
-    // TODO: Set the text color to GREEN using the library function
     
-    printf("System Status: ONLINE\n");
+    printf("System Status: ");
+    set_text_color(GREEN);
+    printf("ONLINE\n");
+    reset_text_color();
 
-    // TODO: Set the text color to RED using the library function
-    
+    set_text_color(RED);
     printf("Alert: Modular compilation in progress...\n");
+    reset_text_color();
 
-    // TODO: Reset the text color back to default
-    
-    // TODO: Print one last divider line
+    print_divider();
 
     return 0;
 }
